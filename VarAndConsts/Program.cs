27535793625afe0f885fbdf875cd28af05c46462
Luiz Spinelli <luiz.luchi@gmail.com>
2021-08-28 23:15:48 -0300

    first c-sharp project

---
+++ VarAndConsts/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace VarAndConsts
+{
+  class Program
+  {
+    static void Main(string[] argumentos)
+    {
+      const string constante = "uma constante que não muda";
+
+      if (argumentos.Length == 0)
+      {
+        Console.WriteLine("Favor informar um numero");
+        return;
+      }
+      int inteiro;
+      bool teste = int.TryParse(argumentos[0], out inteiro);
+
+      if (teste == false)
+      {
+        Console.WriteLine("O argumento informado não é um inteiro");
+        return;
+      }
+
+      Console.WriteLine(constante);
+      Console.WriteLine("O número informado foi: " + inteiro);
+    }
+  }
+}
